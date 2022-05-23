94db2da4762ea6777a3cd0ab52b754b394279743
Beking <sbqoo@qq.com>
2022-05-23 20:07:38 +0800

    ADD: search_triplets

--- Two Pointers/Squaring a Sorted Array (easy).js
+++ Two Pointers/Squaring a Sorted Array (easy).js
@@ -7,11 +7,14 @@
  function make_squares(arr) {
     const squares = Array(arr.length).fill(0);
     let highestSquareIdx = arr.length - 1;
+
     let left = 0;
     let right = arr.length - 1;
+    
     while (left <= right) {
       let leftSquare = arr[left] * arr[left];
       let rightSquare = arr[right] * arr[right];
+
       if (leftSquare > rightSquare) {
         squares[highestSquareIdx] = leftSquare;
         left++;
---
+++ Two Pointers/Triplet Sum to Zero (medium).js
@@ -0,0 +1,45 @@
+/**
+ * Given an array of unsorted numbers, 
+ * find all unique triplets in it that add up to zero.
+ * 
+ * Time O(N^2)  Space O(N)
+ * 
+ * 先固定第一个数a，然后b、c只能从两边向中间靠（在a之后）。细节条件就是去重处理
+ */
+ function search_triplets(arr) {
+    arr.sort((a, b) => a - b);
+    const triplets = [];
+    for (i = 0; i < arr.length; i++) {
+      // 需要和上一次枚举的数不相同
+      if (i > 0 && arr[i] === arr[i - 1]) { // skip same element to avoid duplicate triplets
+        continue;
+      }
+      search_pair(arr, -arr[i], i + 1, triplets);
+    }
+    return triplets;
+  }
+  
+  function search_pair(arr, target_sum, left, triplets) {
+    let right = arr.length - 1;
+    while (left < right) {
+      const current_sum = arr[left] + arr[right];
+      if (current_sum === target_sum) { // found the triplet
+        triplets.push([-target_sum, arr[left], arr[right]]);
+        left++;
+        right--;
+
+        // 跳过重复值
+        while (left < right && arr[left] === arr[left - 1]) {
+          left++; // skip same element to avoid duplicate triplets
+        }
+        while (left < right && arr[right] === arr[right + 1]) {
+          right--; // skip same element to avoid duplicate triplets
+        }
+      } else if (target_sum > current_sum) {
+        left++; // we need a pair with a bigger sum
+      } else {
+        right--; // we need a pair with a smaller sum
+      }
+    }
+  }
+  

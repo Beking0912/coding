f5f209440dd943e120d6a28f134b2d5d58ea9c58
Beking <sbqoo@qq.com>
2022-05-28 01:50:22 +0800

    ADD: find_duplicate

---
+++ Cyclic Sort/Find the Duplicate Number (easy).js
@@ -0,0 +1,21 @@
+/**
+ * 数组中重复的数据
+ * Find that duplicate number without using any extra space.
+ * O(N) O(1)
+ */
+function find_duplicate(nums) {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] !== i + 1) {
+      const j = nums[i] - 1;
+      if (nums[i] !== nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+      } else {
+        return nums[i];
+      }
+    } else {
+      i++;
+    }
+  }
+  return -1;
+}

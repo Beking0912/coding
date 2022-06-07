40f1982b931e0b11ada60c66afa062eff6727a8c
Beking <sbqoo@qq.com>
2022-06-07 14:41:28 +0800

    ADD: count_rotations_with_duplicates

--- Modified Binary Search/Rotation Count (medium).js
+++ Modified Binary Search/Rotation Count (medium).js
@@ -3,6 +3,8 @@
  *
  * find the index of the minimum element
  * The number of times the minimum element is moved to the right will be equal to the number of rotations.
+ *
+ * O(logN) O(1)
  */
 function count_rotations(arr) {
   let start = 0;
@@ -28,5 +30,50 @@ function count_rotations(arr) {
       end = mid - 1;
     }
   }
+  return 0; // the array has not been rotated
+}
+
+/**
+ * find the rotation count of a sorted and rotated array that has duplicates
+ *
+ * The only difference is that before incrementing start or decrementing end,
+ * we will check if either of them is the smallest number.
+ * 
+ * O(N) O(1)
+ */
+function count_rotations_with_duplicates(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    const mid = Math.floor(start + (end - start) / 2);
+
+    if (mid < end && arr[mid] > arr[mid + 1]) {
+      return mid + 1;
+    }
+    if (mid > start && arr[mid - 1] > arr[mid]) {
+      return mid;
+    }
+
+    // skip one number from both ends if they are not the smallest number
+    if (arr[start] === arr[mid] && arr[end] === arr[mid]) {
+      if (arr[start] > arr[start + 1]) {
+        // if element at start+1 is not the smallest
+        return start + 1;
+      }
+      start++;
+      if (arr[end - 1] > arr[end]) {
+        // if the element at end is not the smallest
+        return end;
+      }
+      end--;
+    } // left side is sorted, so the pivot is on right side
+    else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+      start = mid + 1;
+    } else {
+      // right side is sorted, so the pivot is on the left side
+      end = mid - 1;
+    }
+  }
+
   return 0; // the array has not been rotated
 }

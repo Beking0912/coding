31df076e8f799eddc635bf3f40aafdb13cf9eb2b
Beking <sbqoo@qq.com>
2022-06-07 14:51:23 +0800

    ADD: find_single_number

---
+++ Bitwise XOR/Single Number (easy).js
@@ -0,0 +1,12 @@
+/**
+ * In a non-empty array of integers, every number appears twice except for one, find that single number.
+ * 
+ * O(n) O(1)
+ */
+function find_single_number(arr) {
+  let num = 0;
+  for (i = 0; i < arr.length; i++) {
+    num ^= arr[i];
+  }
+  return num;
+}

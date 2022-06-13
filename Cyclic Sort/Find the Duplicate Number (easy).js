4fb7b26839e40290d37a2d53c98e23ed08b2111d
Beking <sbqoo@qq.com>
2022-06-13 19:03:17 +0800

    ADD: find_duplicate

--- Cyclic Sort/Find the Duplicate Number (easy).js
+++ Cyclic Sort/Find the Duplicate Number (easy).js
@@ -2,7 +2,7 @@
  * 数组中重复的数据
  * Find that duplicate number without using any extra space.
  * O(N) O(1)
- * 
+ *
  * https://leetcode.cn/problems/find-the-duplicate-number/
  */
 function find_duplicate(nums) {
@@ -20,4 +20,38 @@ function find_duplicate(nums) {
     }
   }
   return -1;
+}
+
+// 2
+function find_duplicate(nums) {
+  let left = 1;
+  let right = nums.length;
+  while (left < right) {
+    let mid = (right - left) / 2 + left;
+    let count = 0;
+    for (let num of nums) {
+      if (num <= m) ++count;
+    }
+
+    if (count <= m) left = mid + 1;
+    else right = mid;
+  }
+  return 1;
+}
+
+// 3
+function find_duplicate(nums) {
+  let slow = 0;
+  let fast = 0;
+  while (true) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+    if (slow === fast) break;
+  }
+  fast = 0;
+  while (fast !== slow) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
 }
